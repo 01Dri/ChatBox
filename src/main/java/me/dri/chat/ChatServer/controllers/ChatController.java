b9ba8af07993ef83f0585f2b58a0648fbd762cd3
01Dri <drikill01@gmail.com>
2023-09-10 19:57:05 -0300

    Classe ChatController implementada, com ela vamos poder enviar mensagens para o canal correspondente ao websocket

--- src/main/java/me/dri/chat/ChatServer/controllers/ChatController.java
+++ src/main/java/me/dri/chat/ChatServer/controllers/ChatController.java
@@ -1,6 +1,7 @@
 package me.dri.chat.ChatServer.controllers;
 
 
+import me.dri.chat.ChatServer.entities.User;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -8,5 +9,12 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController
 {
+
+    @MessageMapping("/chatMessage")
+    @SendTo("/channel")
+
+    public User sendMessage(User user) {
+        return user;
+    }
 
 }
